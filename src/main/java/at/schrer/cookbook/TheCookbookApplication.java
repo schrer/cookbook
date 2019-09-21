52dd2de86c930c5141d3a2f41ef40a448b5d924c
Martin Schröder <ma.schroeder@outlook.com>
2019-09-21 21:45:25 +0200

    Initial Commit of base Spring Boot project

---
+++ src/main/java/at/schrer/cookbook/TheCookbookApplication.java
@@ -0,0 +1,13 @@
+package at.schrer.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCookbookApplication.class, args);
+	}
+
+}
