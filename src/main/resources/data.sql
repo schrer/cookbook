614b436ac289e300cbade3d66fdfa26da8a273b4
Martin Schröder <ma.schroeder@outlook.com>
2019-10-02 21:01:40 +0200

    Add some recipes to initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,10 @@ INSERT INTO Category (title, description) VALUES ('Nachspeisen','Blabla2');
 INSERT INTO Category (title, description) VALUES ('Suppen','Blabla3');
 INSERT INTO Category (title, description) VALUES ('Saucen','Blabla4');
 INSERT INTO Category (title, description) VALUES ('Getränke','Blabla5');
+
+INSERT INTO Recipe (title, instructions, category) VALUES ('Schnitzel','Fleisch ind Panier, danoch ind Pfann, Zitrone drauf, Preiselbeer daneben potzn, passt','1');
+INSERT INTO Recipe (title, instructions, category) VALUES ('Palatschinken','Teig machen, dünn in die Pfanne, braten, Marmelade/Eis drauf, einrollen, passt','2');
+INSERT INTO Recipe (title, instructions, category) VALUES ('Nudelsuppe','Suppe machen, Nudeln machen, zusammen ins Teller, passt','3');
+INSERT INTO Recipe (title, instructions, category) VALUES ('Sauce Hollandaisè','Unmengen geschmolzener Butter mit Eigelb vermischen, etwas Zitrone, vl. Salz, passt','4');
+INSERT INTO Recipe (title, instructions, category) VALUES ('Old Fashioned','Whisky, Zitronensaft, Eiswürfel, passt','5');
+INSERT INTO Recipe (title, instructions, category) VALUES ('Spaghetti Carbonara','Spaghetti ins Wasser, Speck anbraten, Schlagobers drauf(nicht schlagen!), Eier+Petersilie+Parmesan mischen und zu Schlag geben, Soße auf die Nudeln, passt','1');
