185c4d8091e74d5839122570f6aca184769df49e
Martin Schröder <ma.schroeder@outlook.com>
2020-04-18 22:41:07 +0200

    More documentation for the UrlResolver

--- src/main/java/at/schrer/cookbook/frontend/util/UrlResolver.java
+++ src/main/java/at/schrer/cookbook/frontend/util/UrlResolver.java
@@ -5,7 +5,7 @@ import at.schrer.cookbook.data.dto.RecipeModel;
 import org.springframework.stereotype.Component;
 
 /**
- * Resolves URLs to the view of supported Model-types.
+ * Resolves URLs to the view of supported Model-types and to .
  * All attributes (except the url itself) need to be set on the model before calling the resolve method, otherwise a successful resolving cannot be guaranteed.
  */
 @Component
@@ -15,6 +15,9 @@ public class UrlResolver {
     private static final String SEGMENT_RECIPE = "recipes";
     private static final String SEGMENT_ADD = "add";
 
+    /**
+     * An enum that represents a specific page on the application. Can be resolved to a URL by the {@link UrlResolver}.
+     */
     public enum Path {
         ADD_RECIPE("/" + SEGMENT_RECIPE + "/" + SEGMENT_ADD),
         ADD_CATEGORY("/" + SEGMENT_CATEGORY + "/" + SEGMENT_ADD),
@@ -30,18 +33,43 @@ public class UrlResolver {
         }
     }
 
+    /**
+     * Resolves the view-URL to a category.
+     * All attributes (except the url itself) need to be set on the model before calling the resolve method, otherwise a successful resolving cannot be guaranteed.
+     *
+     * @param category the category model
+     * @return the URL to the view page of the category
+     */
     public String resolve(CategoryModel category) {
         return buildUrlFromSegments(SEGMENT_CATEGORY, Long.toString(category.getId()));
     }
 
+    /**
+     * Resolves the view-URL to a recipe.
+     * All attributes (except the url itself) need to be set on the model before calling the resolve method, otherwise a successful resolving cannot be guaranteed.
+     *
+     * @param recipe the recipe model
+     * @return the URL to the view page of the recipe
+     */
     public String resolve(RecipeModel recipe) {
         return buildUrlFromSegments(SEGMENT_RECIPE, Long.toString(recipe.getId()));
     }
 
+    /**
+     * Resolves the URL for one of the path enums.
+     * @param path a Path enum, that represents the desired URL
+     * @return the URL described by the Path enum
+     */
     public String resolve(Path path){
         return path.getValue();
     }
 
+    /**
+     * Takes an arbitrary number of segments and adds them up to a URL. If no segments are provided the link to the homepage is returned.
+     *
+     * @param segments the segments of the URL
+     * @return the segments merged into a URL, "/" if no segments are provided
+     */
     private String buildUrlFromSegments(String... segments){
         if (segments.length == 0){
             return "/";
